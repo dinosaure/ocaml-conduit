7b8e8e57bd096f4d671edf5f70d2ddf64bf6620c
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-06-22 18:23:41 +0200

    .

--- bench/cost.ml
+++ bench/cost.ml
@@ -9,46 +9,84 @@ end
 
 module Tuyau = Conduit.Make (None) (Bytes) (String)
 
-module Fake_protocol = struct
+let t1 = ref 0L
+
+module Fake_protocol0 = struct
   type input = bytes and output = string and +'a s = 'a
 
-  type endpoint = unit
-  type flow = int64 ref
+  type endpoint = Unix.file_descr
+  type flow = Unix.file_descr
   type error = |
 
   let pp_error : error Fmt.t = fun _ppf -> function _ -> .
-  let connect () = Ok (ref 0L)
-  let recv v _ = v := tick () ; Ok `End_of_flow
-  let send v _ = v := tick () ; Ok 0
+  let connect x = Ok x
+  let recv _ _ = Ok `End_of_flow
+  let send fd v = t1 := tick () ; let _ = Unix.write_substring fd v 0 (String.length v) in Ok 0
   let close _ = Ok ()
 end
 
-let fake = Tuyau.register ~protocol:(module Fake_protocol)
+module Fake_protocol1 = struct
+  type input = bytes and output = string and +'a s = 'a
 
-module Repr = (val Tuyau.repr fake)
+  type endpoint = Unix.file_descr
+  type flow = Unix.file_descr
+  type error = |
 
-let with_tuyau () =
+  let pp_error : error Fmt.t = fun _ppf -> function _ -> .
+  let connect x = Ok x
+  let recv _ _ = Ok `End_of_flow
+  let send _ _ = Ok 0
+  let close _ = Ok ()
+end
+
+module Fake_protocol2 = struct
+  type input = bytes and output = string and +'a s = 'a
+
+  type endpoint = Unix.file_descr
+  type flow = Unix.file_descr
+  type error = |
+
+  let pp_error : error Fmt.t = fun _ppf -> function _ -> .
+  let connect x = Ok x
+  let recv _ _ = Ok `End_of_flow
+  let send _ _ = Ok 0
+  let close _ = Ok ()
+end
+
+let fake0 = Tuyau.register ~protocol:(module Fake_protocol0)
+let fake1 = Tuyau.register ~protocol:(module Fake_protocol1)
+let fake2 = Tuyau.register ~protocol:(module Fake_protocol2)
+let hello_world = "Hello World!\n"
+
+let fully_abstr () =
   let open Rresult in
-  Tuyau.connect () fake >>= function
-  | Repr.T (Conduit.Value v) as flow ->
-    let t0 = tick () in
-    Tuyau.send flow "Hello World!" >>= fun _len ->
-    R.ok (Int64.sub !v t0)
-  | _ -> R.error_msgf "Impossible to deconstruct [fake]"
+  Tuyau.connect Unix.stdout fake0 >>= fun flow ->
+  let t0 = tick () in
+  Tuyau.send flow hello_world >>= fun _len ->
+  let t2 = tick () in
+  R.ok (Int64.sub !t1 t0, Int64.sub t2 !t1)
 
-let without_tuyau () =
+let abstr () =
   let open Rresult in
-  let module Protocol = (val Tuyau.impl fake) in
-  Protocol.connect ()
+  let module Protocol = (val Tuyau.impl fake0) in
+  Protocol.connect Unix.stdout
   |> R.reword_error (R.msgf "%a" Protocol.pp_error) >>= fun flow ->
   let t0 = tick () in
-  Protocol.send flow "Hello World!"
+  Protocol.send flow hello_world
   |> R.reword_error (R.msgf "%a" Protocol.pp_error) >>= fun _len ->
-  R.ok (Int64.sub !flow t0)
+  let t2 = tick () in
+  R.ok (Int64.sub !t1 t0, Int64.sub t2 !t1)
 
-let () = match without_tuyau (), with_tuyau () with
-  | Ok ts0, Ok ts1 ->
-    Fmt.pr "without conduit: %Ld tick(s).\n%!" ts0 ;
-    Fmt.pr "with conduit: %Ld tick(s).\n%!" ts1
-  | Error err, _ -> Fmt.epr "%a.\n%!" Tuyau.pp_error err
-  | _, Error _ -> assert false (* safe *)
+let concrete () =
+  let t0 = tick () in
+  let _ = Unix.write_substring Unix.stdout hello_world 0 (String.length hello_world) in
+  let t1 = tick () in
+  Ok (Int64.sub t1 t0)
+
+let () = match concrete (), fully_abstr (), abstr () with
+  | Ok ts, Ok (ts0, ts1), Ok (tsa, tsb) ->
+    Fmt.pr "fully-abstr: %Ldns, %Ldns.\n%!" ts0 ts1 ;
+    Fmt.pr "abstr: %Ldns, %Ldns.\n%!" tsa tsb ;
+    Fmt.pr "concrete: %Ldns.\n%!"  ts
+  | _ -> assert false
+
